4a122ad252afc44317a4c7d922145fce23389288
Selecia aaa <garrisonroniece@gmail.com>
2023-08-31 03:40:20 -0400

    replaced var with const

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
 // set up event listener
-// Add variables 
+// Add variables... replace them with consts
 // Make Api requests and extract info
 // Use local storage to save the search history
 // function to search wether for a city
 
-var apiKey = "6ef4bd0368a9fb8724b29006f8799de1";
+const apiKey = "6ef4bd0368a9fb8724b29006f8799de1";
 
-var searchForm = document.getElementById("search-form");
-var cityInput = document.getElementById("city-input");
-var currentWeatherInfo = document.getElementById("current-weather-info");
-var forecastInfo = document.getElementById("forecast-info");
-var historyList = document.getElementById("history-list");
-var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+const searchForm = document.getElementById("search-form");
+const cityInput = document.getElementById("city-input");
+const currentWeatherInfo = document.getElementById("current-weather-info");
+const forecastInfo = document.getElementById("forecast-info");
+const historyList = document.getElementById("history-list");
+
 
