9574ac705ebbf8c09d2c31c6648b0fd2151100f9
Selecia aaa <garrisonroniece@gmail.com>
2023-08-31 03:33:13 -0400

    vars added

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,14 @@
 // Add variables 
 // Make Api requests and extract info
 // Use local storage to save the search history
+// function to search wether for a city
+
+var apiKey = "6ef4bd0368a9fb8724b29006f8799de1";
+
+var searchForm = document.getElementById("search-form");
+var cityInput = document.getElementById("city-input");
+var currentWeatherInfo = document.getElementById("current-weather-info");
+var forecastInfo = document.getElementById("forecast-info");
+var historyList = document.getElementById("history-list");
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
